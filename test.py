b6300a506358c27d5b602387074f9447226e82c0
mahmoudjadid <mahmoudjadid@outlook.com>
2025-02-13 14:10:54 +0200

    test succeded

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 age = int(input("please enter age"))
-if age < 8:
-    print ("ticket is 5$")
-else:
-    print ("ticket is 8$")
+if age > 18:
+    print ("ticket is 15$")
+elif age > 12:
+    print ("ticket is 12$")
+# elif age > 8:
+#     print ("ticket is 8$")
+
+
