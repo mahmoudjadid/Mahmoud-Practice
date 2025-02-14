7deba8513618757eb4178bd2948a9451f25cedf6
mahmoudjadid <mahmoudjadid@outlook.com>
2025-02-14 15:34:09 +0200

    the order is working now

--- login.py
+++ login.py
@@ -1,9 +1,9 @@
-username = input("please enter username")
-password = input("please enter password")
+username = input("please enter username: ")
+password = input("please enter password: ")
 
 u="admin"
 p="1234"
-if username == u: & password == p:
+if username == u and password == p:
    print ("acces granted")
-   else 
+else :
    print ("acces dnied")
