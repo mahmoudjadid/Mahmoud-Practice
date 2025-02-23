97f9805bb2108bbc0aa6c9707bd094bd63ff4145
mahmoudjadid <mahmoudjadid@outlook.com>
2025-02-23 22:09:22 +0200

    nabiha-got-salaryraise

--- NabihaSalary.py
+++ NabihaSalary.py
@@ -5,6 +5,8 @@ month = input("please enter month name")
 savings = float(input("please enter percentage of savings"))
 electricity = float(input("please enter percentage of electricity"))
 rent = float(input("please enter percentage of rent"))
+xtrasavings=float(input("please enter the extra amount saved, if none enter 0"))
+salaryup = bool("is there a raise in salary ?")
 
 s = (salary*savings)/100
 e = (electricity*salary)/100
@@ -20,6 +22,17 @@ def expences():
 def yearly():
     yearlye=e*12
     yearlyr=r*12
-    totalyearexp=yearlye+yearlyr9    print (f"your total expences per year {totalyearexp}")
+    totalyearexp=yearlye+yearlyr9    
+    print (f"your total expences per year {totalyearexp}")
+
+def xtsavper():
+    xtsavper= (xtrasavings/savings)/100
+    print (f"your extra saving percentage is {xtsavper}")
     
+def raiseinsalary():
+    
+    if salaryup==true:
+        newsalary=salary**2    
+        print("the new salary is", {newsalary})
+        
 expences()
