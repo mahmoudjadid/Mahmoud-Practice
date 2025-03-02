aed2eac81ed90f3b435798072bed80a1c6ee4ce2
mahmoudjadid <mahmoudjadid@outlook.com>
2025-03-02 22:27:58 +0200

    modified-vergin

--- car_rent_system.py
+++ car_rent_system.py
@@ -1,33 +1,23 @@
-
-
 class vehicle:
     
-    name= None
-    year=0
-    seats=0
-    rentprice=0.0
-    engcapa=0.0
-    
-    def __init__(self, name, year, seats, rentprice, engcapa):
+    def __init__(self, name, year, rentprice, engcapa, seats):
         self.name = name
         self.year=year
-        self.seats=seats
+        self.seats=seats    
         self.rentprice=rentprice
         self.engcapa=engcapa
- 
     
-    
-    
-veh1=vehicle("BMW", 2020, 4, 40 ,120)
-veh2=vehicle("KIA", 2022, 5, 30, 90)
-veh3=vehicle("toyota", 2019, 7, 35, 130)
-
-
-def days():
-    input=("enter the number of days: ")
-
-def totalrent():
-    totalrent=vehicle.rentprice*days    
-    return totalrent
-
-print(veh1.name)
+    def totalrent(self):
+        carname=input("enter car name: ")
+        days=int(input("enter the number of days: "))
+        if (carname=="BMW"):
+            totalrent=veh1.rentprice*days
+        elif (carname=="KIA"):
+            rentprice=veh2.rentprice
+        print(self.rentprice * days)
+            
+veh1=vehicle("BMW", 2020, 40, 4 ,120)
+veh2=vehicle("KIA", 2022, 30, 5, 90)
+veh3=vehicle("toyota", 2019, 35, 7, 130)
+      
+veh1.totalrent()
--- test.py
+++ test.py
@@ -1,9 +1,23 @@
-age = int(input("please enter age"))
-if age > 18:
-    print ("ticket is 15$")
-elif age > 12:
-    print ("ticket is 12$")
-elif age > 8:
-     print ("ticket is 8$")
-     
-
+class vehicle:
+    
+    def __init__(self, name, year, rentprice, engcapa, seats):
+        self.name = name
+        self.year=year
+        self.seats=seats    
+        self.rentprice=rentprice
+        self.engcapa=engcapa
+    
+    def totalrent(self):
+        carname=input("enter car name: ")
+        days=int(input("enter the number of days: "))
+        if (carname=="BMW"):
+            totalrent=veh1.rentprice*days
+        elif (carname=="KIA"):
+            rentprice=veh2.rentprice
+        print(self.rentprice * days)
+            
+veh1=vehicle("BMW", 2020, 40, 4 ,120)
+veh2=vehicle("KIA", 2022, 30, 5, 90)
+veh3=vehicle("toyota", 2019, 35, 7, 130)
+      
+veh1.totalrent()
