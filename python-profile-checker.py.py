7ad269cd1f5b578e1ff7d4579bba316b332be040
mahmoudjadid <mahmoudjadid@outlook.com>
2025-06-01 17:58:49 +0300

    working well

--- python-profile-checker.py.py
+++ python-profile-checker.py.py
@@ -7,6 +7,14 @@ foi=input("enter your feild of interest: ")
 grad=bool(input("are you graduated ?"))
 
 
+if age > 25 and gpa >=3.5 and grad==True:
+    print("you are eligible for scholarship")
+elif age > 30 and gpa >= 2.5 :
+    print("your are eligible for internship")
+else:
+    print("we recommend you to apply later")
 
 
 
+
+#   >  <
