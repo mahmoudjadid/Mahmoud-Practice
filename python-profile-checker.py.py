2aa10680cab00677bfe7472e368a6085e561a3b0
mahmoudjadid <mahmoudjadid@outlook.com>
2025-06-01 17:47:07 +0300

    inputs trail

--- python-profile-checker.py.py
+++ python-profile-checker.py.py
@@ -1,9 +1,10 @@
 
+
 name=str(input("enter your name: "))
 age=int(input("enter your age:"))
 gpa=float(input("enter your GPA: "))
-
-
+foi=input("enter your feild of interest: ")
+grad=bool(input("are you graduated ?"))
 
 
 
